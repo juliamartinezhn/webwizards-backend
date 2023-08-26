19cfbe6a0083e3c06c3428340f367a403a147ec5
sramador45 <58308854+sramador45@users.noreply.github.com>
2023-08-26 01:11:39 -0600

    Commit  mongo Atlas DB
    creacion de base de datos mongo atlas

--- modules/database.js
+++ modules/database.js
@@ -3,15 +3,22 @@ var mongoose = require('mongoose');
 let db = 'webwizards';
 let port = '27017';
 let host = '127.0.0.1';
+const dbURI = 'mongodb+srv://yovanniamador:FGxGGS4kJscJkvHU@cluster0.b8lblk3.mongodb.net/webwizards?retryWrites=true&w=majority';
+
 
 class Database {
     constructor() {
         this.conectar();
     }
-    
+
     conectar() {
-        mongoose.connect(`mongodb://${host}:${port}/${db}`)
-            .then(result => console.log('Se conectó a mongodb'))
+        // mongoose.connect(`mongodb://${host}:${port}/${db}`)
+        //     .then(result => console.log('Se conectó a mongodb'))
+        //     .catch(error => console.log(error));
+        mongoose.connect(dbURI,{
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }).then(result => console.log('Se conectó a mongodb'))
             .catch(error => console.log(error));
     }
 }
